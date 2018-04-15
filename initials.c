22d2a670e15ec195d8f7039d6d537de6e3335502
Candice Bianca Brown <candicebrown97@gmail.com>
2018-04-15 12:26:17 +0000

    initial commit

---
+++ initials.c
@@ -0,0 +1,27 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    char array1 = get_string();
+    char array2 = get_string();
+    char array3 = get_string();
+
+
+
+
+}
+
+
+// Specification
+// Design and implement a program, initials, that, given a person’s name, prints a person’s initials.
+
+// Implement your program in a file called initials.c in a directory called initials.
+
+// Your program should prompt a user for their name using get_string to obtain their name as a string.
+
+// You may assume that the user’s input will contain only letters (uppercase and/or lowercase) plus single spaces between words. You don’t need to worry about names like Joseph Gordon-Levitt, Conan O’Brien, or David J. Malan!
+
+// Your program should print the user’s initials (i.e., the first letter of each word in their name) with no spaces or periods, followed by a newline (\n).
+
+// letters to be printed using toUpper no matter what case is entered by the user
