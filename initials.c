02e6e676aedccc74ad0c8bf6cb91733c64a82257
Candice Bianca Brown <candicebrown97@gmail.com>
2018-04-16 16:34:20 +0000

    working version - currently printing my first initial as a capital letter, no matter case entered - will continue on with remaining letters

--- initials.c
+++ initials.c
@@ -1,17 +1,29 @@
 #include <cs50.h>
 #include <stdio.h>
+#include <string.h> //You can get the length of a string using the strlen function. This function is declared in the header file string.h.
+#include <ctype.h> //The ctype.h header file of the C Standard Library declares several functions that are useful for testing and mapping characters
+
+// int main(int argc, char ** argv); //int main(int argc, string argv) does not work either
 
 int main(void)
 {
-    char array1 = get_string();
-    char array2 = get_string();
-    char array3 = get_string();
+    string userName = get_string("What's Your Name?: "); //declares data type string for variable named userName -- user name is the string entered by the user
 
+    for (int initial = 0, sLength = strlen(userName); initial < sLength; initial++) //to loop over each element of the string - using variable called initial as the counter
+    //initialize counter to 0, and obtain the length of userName to store value in sLength; 0 less than length of userName; increase counter by 1
+    {
+        if(initial == 0 && isalpha(userName[initial])) //checking the first position to make sure it's a letter
+            {
+                printf("%c", toupper(userName[initial])); //prints the first letter as uppercase
+            }
+
+    }
+
+    printf("\n"); //print new line
 
 
 
 }
-
 
 // Specification
 // Design and implement a program, initials, that, given a person’s name, prints a person’s initials.
