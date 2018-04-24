84d0adf330d866cbd507b5a51ddb8dcf97721d27
Candice Bianca Brown <candicebrown97@gmail.com>
2018-04-24 08:53:37 +0000

    initial commit for caesar - library set up

---
+++ Caesar/caesar.c
@@ -0,0 +1,8 @@
+#include <cs50.h>
+#include <stdio.h>
+#include <stdlib.h> //contains atoi function: Its prototype is as follows: int atoi(const char *str); The str argument is a string, represented by an array of characters, containing the characters of a signed integer number.
+#include <ctype.h> //The ctype.h header file of the C Standard Library declares several functions that are useful for testing and mapping characters
+#include <string.h> //You can get the length of a string using the strlen function. This function is declared in the header file string.h.
+
+
+
